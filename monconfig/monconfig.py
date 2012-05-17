c76a69fe6ad332c568d32ada1103b950a82647fe
Jeremy Hanmer <jeremy@dreamhost.com>
2012-05-17 14:56:33 -0700

    Change-Id: 9f1d6fbd3ffe2f484fa6ce150bb5cc27ce2e18bf

--- monconfig/monconfig.py
+++ monconfig/monconfig.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python
+
+from mako.template import Template
+import yaml
+
+yamlfile = open('testconfig.yaml','r')
+
+hostinfo = yaml.load(yamlfile)
+print hostinfo
+
+targettemplate = Template(filename='templates/smokeping/Targets.tmpl')
+
+smokeconfig = targettemplate.render(hostinfo=hostinfo)
+print smokeconfig
+
+smokefile = open('/etc/smokeping/config.d/Targets','w');
+smokefile.write(smokeconfig)
+smokefile.close
+
